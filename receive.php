07a523e1f1c99781f9ae8491fcde028e7d35551e
ninolin <a7872278722@gmail.com>
2018-01-09 11:36:30 +0800

    get line message

--- receive.php
+++ receive.php
@@ -1,3 +1,9 @@
 <?php
- echo "hello i'm nino"
+ $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
+ $json_obj = json_decode($json_str); //轉JSON格式
+
+ $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
+ fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入\xEF\xBB\xBF轉成utf8格式
+ fclose($myfile);
+
 ?>
