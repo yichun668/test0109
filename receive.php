20a6eb8a0cd1ffbd63278c6616b5638fb2cf04a2
yichun668 <35246412+yichun668@users.noreply.github.com>
2018-01-09 16:15:17 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,39 +1,134 @@
 <?php
- $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
- $json_obj = json_decode($json_str); //轉JSON格式
+	//接收request的body(可以接收除了Content-type為multipart/form-data的資料)
+	$json_str = file_get_contents('php://input'); 
+	$json_obj = json_decode($json_str); //轉成json格式
+	
+	$myfile = fopen("log.txt", "w+") or die("Unable to open file!"); //設定一個log.txt，用來印訊息
+	fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加上\xEF\xBB\xBF轉成utf8格式
 
-$myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
- fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入\xEF\xBB\xBF轉成utf8格式
- fclose($myfile);
- //產生回傳給line server的格式
- $sender_userid = $json_obj->events[0]->source->userId;
- $sender_txt = $json_obj->events[0]->message->text;
- $sender_replyToken = $json_obj->events[0]->replyToken;
- $line_server_url = 'https://api.line.me/v2/bot/message/push';
- //用sender_txt來分辨要發何種訊息
- $objID = $json_obj->events[0]->message->id;
+	//產生回傳給line server的格式
+	$sender_userid = $json_obj->events[0]->source->userId;
+	$sender_txt = $json_obj->events[0]->message->text;
+	$sender_replyToken = $json_obj->events[0]->replyToken;
+	$line_server_url = 'https://api.line.me/v2/bot/message/push';
+	switch ($sender_txt) {
+    		case "push":
+        		$response = array (
+				"to" => $sender_userid,
+				"messages" => array (
+					array (
+						"type" => "text",
+						"text" => "Hello, YOU SAY ".$sender_txt
+					)
+				)
+			);
+        		break;
+    		case "reply":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "text",
+						"text" => "Hello, YOU SAY ".$sender_txt
+					)
+				)
+			);
+        		break;
+		case "image":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "image",
+						"originalContentUrl" => "https://www.w3schools.com/css/paris.jpg",
+						"previewImageUrl" => "https://www.nasa.gov/sites/default/themes/NASAPortal/images/feed.png"
+					)
+				)
+			);
+        		break;
+		case "location":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "location",
+						"title" => "my location",
+						"address" => "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+            					"latitude" => 35.65910807942215,
+						"longitude" => 139.70372892916203
+					)
+				)
+			);
+        		break;
+		case "sticker":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "sticker",
+						"packageId" => "1",
+						"stickerId" => "1"
+					)
+				)
+			);
+        		break;
+		case "button":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "template",
+						"altText" => "this is a buttons template",
+						"template" => array (
+							"type" => "buttons",
+							"thumbnailImageUrl" => "https://www.w3schools.com/css/paris.jpg",
+							"title" => "Menu",
+							"text" => "Please select",
+							"actions" => array (
+								array (
+									"type" => "postback",
+									"label" => "Buy",
+									"data" => "action=buy&itemid=123"
+								),
+								array (
+									"type" => "postback",
+                   							"label" => "Add to cart",
+                    							"data" => "action=add&itemid=123"
+								)
+							)
+						)
+					)
+				)
+			);
+        		break;
+    		default:
+			$objID = $json_obj->events[0]->message->id;
 			$url = 'https://api.line.me/v2/bot/message/'.$objID.'/content';
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 				'Authorization: Bearer 9knlJRZBbsuxmgFxx0cpBMCTp8AmWS2eaXaO81WxBTUE0Z74Sjnq6lJXd+g86BP7w4Pe+aH/O4eiZcfgOMQuAISQX+FOgxDNHgyxxX8GTgTfqOW+v2/HA5qJ1YFusjV8OhsHuskj4mgIgZX1Eotv0QdB04t89/1O/w1cDnyilFU=";
-				//n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=',
+				//cCubKq3mCMRx0RcZcoHLDP0r38pPEn5ZkqgTRT0c4fexsmrtN52Fs5kGkQxZYmED5pM1iDsG5M+1si8PS5dgKDs8xF6Qw0DNdddVrMkhc9WJmD1pRVtGqwY4rSNS+/AgkfGoI10hRps8GI//6k7f9AdB04t89/1O/w1cDnyilFU=',
 			));
 				
 			$json_content = curl_exec($ch);
 			curl_close($ch);
-$imagefile = fopen($objID.".jpeg", "w+") or die("Unable to open file!"); //設定一個log.txt，用來印訊息
+
+			$imagefile = fopen($objID.".jpeg", "w+") or die("Unable to open file!"); //設定一個log.txt，用來印訊息
 			fwrite($imagefile, $json_content); 
 			fclose($imagefile);
-
-
-$header[] = "Content-Type: application/json";
+        		$header[] = "Content-Type: application/json";
 			$post_data = array (
 				"requests" => array (
 						array (
 							"image" => array (
 								"source" => array (
-									"imageUri" => "http://139.59.123.8/chtChatBot/nino0109/".$objID.".jpeg"
+									"imageUri" => "http://139.59.123.8/chtChatBot/20180109_LineBot/".$objID.".jpeg"
 								)
 							),
 							"features" => array (
@@ -51,34 +146,39 @@ $header[] = "Content-Type: application/json";
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
 			$result = json_decode(curl_exec($ch));
-			/*$result_ary = mb_split("\n",$result -> responses[0] -> fullTextAnnotation -> text);
+			$result_ary = mb_split("\n",$result -> responses[0] -> fullTextAnnotation -> text);
 			$ans_txt = "這張發票沒用了，你又製造了一張垃圾";
 			foreach ($result_ary as $val) {
 				if($val == "AG-26272435"){
 					$ans_txt = "恭喜您中獎啦，快分紅!!";
 				}
-			}*/
+			}
 			$response = array (
 				"to" => $sender_userid,
 				"messages" => array (
 					array (
 						"type" => "text",
-						"text" => $result -> responses[0] -> fullTextAnnotation -> text
+						"text" => $ans_txt
 					)
 				)
 			);
+        		break;
+	}
 
-
- //回傳給line server
- $header[] = "Content-Type: application/json";
- $header[] = "Authorization: Bearer 9knlJRZBbsuxmgFxx0cpBMCTp8AmWS2eaXaO81WxBTUE0Z74Sjnq6lJXd+g86BP7w4Pe+aH/O4eiZcfgOMQuAISQX+FOgxDNHgyxxX8GTgTfqOW+v2/HA5qJ1YFusjV8OhsHuskj4mgIgZX1Eotv0QdB04t89/1O/w1cDnyilFU=";
- //n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=";
- $ch = curl_init($line_server_url);                                                                      
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
- curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
- curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
- $result = curl_exec($ch);
- curl_close($ch); 
-
+	
+	
+	//fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前加上\xEF\xBB\xBF轉成utf8格式
+	fclose($myfile);
+		
+	//回傳給line server
+	$header[] = "Content-Type: application/json";
+	$header[] = "Authorization: Bearer 9knlJRZBbsuxmgFxx0cpBMCTp8AmWS2eaXaO81WxBTUE0Z74Sjnq6lJXd+g86BP7w4Pe+aH/O4eiZcfgOMQuAISQX+FOgxDNHgyxxX8GTgTfqOW+v2/HA5qJ1YFusjV8OhsHuskj4mgIgZX1Eotv0QdB04t89/1O/w1cDnyilFU=";
+	//cCubKq3mCMRx0RcZcoHLDP0r38pPEn5ZkqgTRT0c4fexsmrtN52Fs5kGkQxZYmED5pM1iDsG5M+1si8PS5dgKDs8xF6Qw0DNdddVrMkhc9WJmD1pRVtGqwY4rSNS+/AgkfGoI10hRps8GI//6k7f9AdB04t89/1O/w1cDnyilFU=";
+	$ch = curl_init($line_server_url);                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
+	$result = curl_exec($ch);
+	curl_close($ch);
 ?>
